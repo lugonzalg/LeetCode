3fdb4639194dd19af5a0fde4dcdb1d25aee6a5b2
lugonzalg <glukas94@gmail.com>
2023-10-24 19:24:05 +0200

    CHORE: find duplicate

---
+++ find_duplicate/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        std::map<int, int>::iterator    it_list;
+        std::vector<int>::iterator      it;
+
+        for (it = nums.begin(); it < nums.end(); it++) {
+
+            it_list = this->_num_list.find(*it);
+            if (it_list != this->_num_list.end())
+                return true;
+            else
+                this->_num_list[*it] = 1;
+
+        }
+        return false;
+    }
+private:
+    std::map<int, int>  _num_list;
+};
